53992e018c84ba6fdf5806927b7855aad912e047
Alejandro Ortega <alexaon.ao@gmail.com>
2026-02-24 20:39:49 +0100

    9

--- script.js
+++ script.js
@@ -9,37 +9,3 @@ document.addEventListener('mousemove', e => {
     `translate(${x}px,${y}px)`;
 
 });
-
-
-// Form animation
-
-const form = document.getElementById("contactForm");
-
-form.addEventListener("submit", e => {
-
-  e.preventDefault();
-
-  const btn = form.querySelector("button");
-  const old = btn.innerText;
-
-  btn.innerText = "Enviando...";
-  btn.disabled = true;
-
-  setTimeout(()=>{
-
-    btn.innerText = "Enviado ✓";
-    btn.style.background = "#3cff9b";
-
-    setTimeout(()=>{
-
-      btn.innerText = old;
-      btn.disabled = false;
-      btn.style.background = "";
-
-      form.reset();
-
-    },2000);
-
-  },1500);
-
-});
